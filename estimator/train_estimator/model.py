0ce9e92dd49ef5b5ce684de62c16b2bd356ecff3
srom <romain.strock@gmail.com>
2017-10-21 09:39:58 +0100

    Bump up kappa

--- estimator/train_estimator/model.py
+++ estimator/train_estimator/model.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 INPUT_DIMENSION = 768  # 8 x 8 squares x 12 piece types
 HIDDEN_UNITS = 2048
-KAPPA = 1.0  # Emphasizes f(p) = -f(q)
+KAPPA = 2.0  # Emphasizes f(p) = -f(q)
 INITIAL_LEARNING_RATE = 0.1
 MOMENTUM = 0.9
 DECAY_STEPS = 4e3
