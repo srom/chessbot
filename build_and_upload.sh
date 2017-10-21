b59d5f41212ee395060fc2a15fdd9c5a6ceb3aa2
srom <romain.strock@gmail.com>
2017-10-21 17:56:07 +0100

    Exclude a few more directories

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -2,6 +2,6 @@
 
 TARFILE=chessbot.tar.gz
 tar --exclude '.git*' --exclude '*.sqlite3' --exclude 'env' --exclude 'packages/*' --exclude '.idea/*' \
-    --exclude '*.pyc' -pczvf $TARFILE .
+    --exclude '*.pyc' --exclude 'checkpoints/*' --exclude 'model/*' -pczvf $TARFILE .
 
 aws s3 cp $TARFILE s3://romainstrock-packages/
