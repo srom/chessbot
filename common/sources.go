a91b1f5c1f9ff3b80a06c6012a41a18e51360055
srom <romain.strock@gmail.com>
2017-10-24 21:34:30 +0100

    Update sources with full year all rating datasets

--- common/sources.go
+++ common/sources.go
@@ -3,17 +3,24 @@ package common
 // http://www.ficsgames.org/download.html
 // These URLS expire quickly so they need to be updated often.
 var SOURCES = []string{
-	"http://www.ficsgames.org/dl/ficsgamesdb_2016_chess2000_nomovetimes_1501117.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2015_chess2000_nomovetimes_1501118.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2014_chess2000_nomovetimes_1501119.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2013_chess2000_nomovetimes_1501120.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2012_chess2000_nomovetimes_1501121.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2011_chess2000_nomovetimes_1501122.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2010_chess2000_nomovetimes_1501125.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2009_chess2000_nomovetimes_1501126.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2008_chess2000_nomovetimes_1501127.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2007_chess2000_nomovetimes_1501128.pgn.bz2",
-	"http://www.ficsgames.org/dl/ficsgamesdb_2006_chess2000_nomovetimes_1501123.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2016_chess_nomovetimes_1502841.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2015_chess_nomovetimes_1502842.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2014_chess_nomovetimes_1502844.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2013_chess_nomovetimes_1502845.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2012_chess_nomovetimes_1502846.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2011_chess_nomovetimes_1502847.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2010_chess_nomovetimes_1502848.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2009_chess_nomovetimes_1502849.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2008_chess_nomovetimes_1502850.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2007_chess_nomovetimes_1502851.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2006_chess_nomovetimes_1502852.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2005_chess_nomovetimes_1502853.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2004_chess_nomovetimes_1502854.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2003_chess_nomovetimes_1502855.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2002_chess_nomovetimes_1502856.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2002_chess_nomovetimes_1502856.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2001_chess_nomovetimes_1502857.pgn.bz2",
+	"http://www.ficsgames.org/dl/ficsgamesdb_2000_chess_nomovetimes_1502858.pgn.bz2",
 }
 
 func YieldSourceURLs(done <-chan struct{}) <-chan string {
